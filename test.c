c314e345cb602da9b4e9bdbefc06f59381f0cd9c
Deradera <akiana96@live.co.uk>
2015-12-11 16:06:48 +0000

    Update test.c
    Replaced with mine - has more comments

--- test.c
+++ test.c
@@ -2,73 +2,172 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "blocked_stack_int.h"
-/*************************************************************/
+
 struct node
 {
-	struct node *next; 			//points to next node in stack 
-	int *element;				//creates a pointer to 
-	int top;					//
+	struct node *next;
+	int *items;
+	int top;
 };
-/*************************************************************/
+
 struct stack_int_implementation
 {
 	struct node * top;
 	int size;
 	int block_size;
 };
-/*************************************************************/
-stack_int * new_blocked_stack_int(int block_size){
-	stack_int * s = (stack_int*)malloc(sizeof(block_size));	//allocates memory for the stack 
-	s->top = NULL; 								//this makes it so the top of the stack holds no value on creation							
-	s->size = 0;									//sets the size of the stack to zero
-	s->block_size = block_size;						//assigns block_size and the block size 
-};
 
-
-/*************************************************************/
-int stack_int_isempty(stack_int *s){
-	assert(s!=NULL);								//checks that s is defined  
-	return (s->size == 0); 							// this will returns true if the size is equal to zero
+stack_int * new_blocked_stack_int(int block_size)
+{
+	stack_int * s = (stack_int *)malloc(sizeof(stack_int));
+	s->top = NULL;							//sets top of stack to NULL as there should be no stack yet
+	s->size = 0;							//size should be 0 as nothing is in the stack yet
+	s->block_size = block_size;					//assign block_size as the block size
 }
 
-/*************************************************************/
-int  stack_int_size(stack_int *s){
-	assert(s!=NULL);								//checks that s is defined
-	return (s->size);								//this will return how many items have been added to the stack
+/****************************************************************************
+* ~IsEmpty
+* Parameters
+* s	- pointer to the header node
+* 
+* Description:
+* Checks to see if the stack is empty. Returns true if it is empty.
+*****************************************************************************/
+
+int stack_int_isempty(stack_int *s)
+{
+	assert(s!=NULL);			//check s is defined before dereferencing
+	return (s->size == 0);			//returns true or false depending on whether
+}						//the size of the array is 0 or not.
+
+/****************************************************************************
+* ~Size
+* Parameters
+* s	- pointer to the header node
+* 
+* Description:
+* Returns the size of the stack. It is similar to top.
+*****************************************************************************/
+
+int stack_int_size(stack_int *s)
+{
+	assert(s!=NULL);			//check s is defined before dereferencing
+	return (s->size);			//return the size of the stack
 }
 
-/*************************************************************/
-void stack_int_push(stack_int *s, int x){
-	assert(s!=NULL);								//checks that s is defined
-	if(stack_int_size(s) == (s->block_size)){				//checks to see if the block is full
-		struct node *new_blocked_stack_int;		//creates a new block with a pointer to previous block
+/****************************************************************************
+* ~Push
+* Parameters
+* s	- pointer to the header node
+* x	- value to be pushed onto the stack
+* 
+* Description:
+* Checks if the stack is full first. Then pushes an item onto the stack 
+* if it isn't full. If it is full it shall display an error message and exit
+* the program.
+*****************************************************************************/
+void stack_int_push(stack_int *s, int x)
+{
+	assert(s!=NULL);			//check s is defined before dereferencing
+	if (stack_int_size(s) == (s->block_size)) {
+		struct node->next = new_blocked_stack_int;	//create new block if the size = block size (fuck knows if ive done it right
 	}
-	else{
-		s->top->element[s->top->top] = x;				//this point to next available in the elements array
-		(s->top->top)++;							//this pushs element top down and adds element to top
-		(s->size)++;								//this adds 1 to the size of the stack 
-	}	
+	else {
+		s->top->items[s->top->top] = x;		//points to the next available
+							//space in the s->items array.
+							//make it equal to x (the next item to be added.)
+		(s->top->top)++;			//Go to next free space index
+		(s->size)++;				//increment size so that it is correct
+	}
 }
-/*************************************************************/
-void stack_int_pop(stack_int *s){
-	assert(s!=NULL);								//checks if s is defined 
-	if(stack_int_isempty(s)){							//this calls the stack_int_empty function to see if the stack is empty 
-		printf("No value can be popped as the stack is empty");// if it is empty a error message is provided as no element can be popped
+
+/****************************************************************************
+* ~Pop
+* Parameters
+* s	- pointer to the header node
+* 
+* Description:
+* Checks if the stack is empty first. Then pops the top item off the stack 
+* if it isn't empty. If it is empty it shall display an error message and exit
+* the program.
+*****************************************************************************/
+
+void stack_int_pop(stack_int *s)
+{
+	assert(s!=NULL);				//check s is defined before dereferencing
+	struct node * n;
+	if (stack_int_isempty(s)) {
+		printf("stack_int_pop error: stack is currently empty.\n");
 		exit(1);
 	}
-	else{
-		s->top->element[s->top->top]--;
+	else {
+		/*n = s->top;
+		s->top = s->top->next;
+		free(n);
+		*/
+		
+		s->top->items[s->top->top]--;
+		//(s->top)--;
 		(s->size)--;
 	}
-}		
-/************************************************************/
-int  stack_int_top(stack_int *s){
-	return 0;
 }
-/*************************************************************
-void stack_int_display(stack_int*s){
-assert(s!=NULL);								//checks that s is defined
-}*/
-/*************************************************************
-void stack_int_release(stack_int *s){
-}*/
+
+/****************************************************************************
+* ~Top
+* Parameters
+* s	- pointer to the header node
+* top	- pointer to the top of the stack
+* items	- pointer to the items in the array
+* 
+* Description:
+* Returns the value at the top of the stack. If the stack is empty it will 
+* give an error.
+*****************************************************************************/
+
+int stack_int_top(stack_int *s)
+{
+	assert(s!=NULL);			//check s is defined before dereferencing
+	if (stack_int_isempty(s)) {
+		printf("stack_int_top error: stack is currently empty.\n");
+		exit(1);
+	}
+	else {
+		return (s->top->items);
+	}
+}
+
+/****************************************************************************
+* ~Display
+* Parameters
+* s	- pointer to the header node
+* 
+* Description:
+* Displays the stack.
+*****************************************************************************/
+
+void stack_int_display(stack_int *s)
+{
+	assert(s!=NULL);			//check s is defined before dereferencing
+	int i=0;
+	printf("Stack<<[ ");
+	if (!stack_int_isempty(s)) {
+		while (i<((s->top)-1)) {
+			printf("%i, ", (s->top->items[i]));
+	}
+	printf(" ]");
+}
+
+/****************************************************************************
+* ~Release
+* Parameters
+* s	- pointer to the header node
+* 
+* Description:
+* Frees up the pointer s.
+*****************************************************************************/
+
+void stack_int_release(stack_int *s)
+{
+	assert(s!=NULL);			//check s is defined before dereferencing
+	free(s);				//frees up the pointer
+}
