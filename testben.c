4467e0caf8db4f525a49c2aef87623d07d181975
DSBVengeance <DSBVengeance@users.noreply.github.com>
2015-12-11 18:09:58 +0000

    Update testben.c

--- testben.c
+++ testben.c
@@ -17,14 +17,15 @@ struct stack_int_implementation
 	struct node * top;			//
 	int size;
 	int block_size;
-}
+};
 /**********************************************************************************/
 
 stack_int * new_blocked_stack_int(int block_size){
 	stack_int * s = (stack_int*)malloc(sizeof(block_size));	//allocates memory for the stack 
-	s->top = NULL; 								//this makes it so the top of the stack holds no value on creation							
+	s->top = 0; 								//this makes it so the top of the stack holds no value on creation							
 	s->size = 0;									//sets the size of the stack to zero
 	s->block_size = block_size;						//assigns block_size and the block size 
+	return s;
 };
 
 /**********************************************************************************/
@@ -40,10 +41,10 @@ int  stack_int_size(stack_int *s){
 
 }
 /**********************************************************************************/
-/*void stack_int_push(stack_int *s, int x){
+void stack_int_push(stack_int *s, int x){
 	assert(s!=NULL);
-	if (stack_int_size(s) == (s->block_size)) {
-		struct node *next -> new_blocked_stack();	//creates new block with pointer to previous block
+	if (s->size == 0 || s->top->top >= (s->block_size)) {
+		struct node *next = (struct node *)malloc(s->block_size * sizeof(int));	//creates new block with pointer to previous block
 	}
 	else {
 		s->top->data[s->top->top] = x;		//points to the next available
@@ -52,12 +53,12 @@ int  stack_int_size(stack_int *s){
 		(s->top->top)++;			//Go to next free space index
 		(s->size)++; 
 	}
-}*/
+}
 
 /**********************************************************************************/
 void stack_int_pop(stack_int *s){
 	assert(s!=NULL);
-	if(stack_int_empty(s)){
+	if(stack_int_isempty(s)){
 		printf("No value can be popped as stack is empty");
 		exit(1);
 	}
@@ -67,17 +68,27 @@ void stack_int_pop(stack_int *s){
 	}
 
 }
-/**********************************************************************************
+/**********************************************************************************/
 int  stack_int_top(stack_int *s){
 
 
 
 }
-/**********************************************************************************
+/**********************************************************************************/
 void stack_int_display(stack_int *s){
-
-   
-
+	assert(s!=NULL);
+	struct node *p = s->top;
+	printf("Stack=/n -----");
+	if(p!=NULL){		
+		while (p->next!=NULL){
+			printf("[");
+			printf("%i ", p->data);
+			p = p->next;
+			printf("]/n");
+		}
+		printf("%i", p->data);
+	}
+	printf("-----------------");
 }
 /**********************************************************************************
 void stack_int_release(stack_int *s)
